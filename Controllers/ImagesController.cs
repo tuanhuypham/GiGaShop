388cd30b84491a2caa3918e997215cc7e64e14c8
tuanhuypham <huy5153@gmail.com>
2024-12-09 18:41:10 +0700

    display picture

---
+++ Controllers/ImagesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Linq;
+
+namespace Gigashop.Controllers
+{
+    [Route("api/images")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpGet("products")]
+        public IActionResult GetProductImages()
+        {
+            var imageFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", "Products");
+            var images = Directory.GetFiles(imageFolder)
+                                  .Select(Path.GetFileName)
+                                  .ToList();
+
+            return Ok(images);
+        }
+    }
+}
